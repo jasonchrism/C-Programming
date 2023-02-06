ff3f039932781578a0ba13f09d0f98cf6ca1b324
Jasonchrism <jasoncm712@gmail.com>
2023-02-06 15:04:40 +0700

    new edited

--- LEC/Data Structure Lec/BinarySearchTree.cpp
+++ LEC/Data Structure Lec/BinarySearchTree.cpp
@@ -4,132 +4,182 @@
 
 struct data{
 	int val;
-	
 	struct data *left;
 	struct data *right;
-} *root = NULL;
+} *root = 0;
 
 struct data *createData(int val){
 	struct data *temp = (struct data*)malloc(sizeof(struct data));
 	temp->val = val;
-	temp->left = temp->right = NULL;
+	temp->left = temp->right = 0;
+	
 	return temp;
 }
 
-void insert(struct data *curr, int newVal){
-	if(root == NULL){
-		root = createData(newVal);
+void insert(struct data *curr, int val){
+	
+	if(root == 0){
+		root = createData(val);
+		return;
 	}
 	else{
-		if(newVal < curr->val && curr->left == NULL){
-			curr->left = createData(newVal);
-			
-		}else if(newVal > curr->val && curr->right == NULL){
-			curr->right = createData(newVal);
-			
-		}else if(newVal < curr->val){
-			insert(curr->left , newVal);
-			
-		}else if(newVal > curr->val){
-			insert(curr->right , newVal);
+		if(val < curr->val && curr->left == 0){
+			curr->left = createData(val);	
+		} 
+		else if(val > curr->val && curr->right == 0){
+			curr->right = createData(val);
 		}
+		else if(val < curr->val){
+			insert(curr->left, val);
+		}
+		else{
+			insert(curr->right, val);	
+		} 
 	}
 }
 
-void displayInfix(struct data *curr){
-	
-	if(curr->left != NULL) displayInfix(curr->left);
-	printf("%d " , curr->val);
-	if(curr->right != NULL) displayInfix(curr->right);
+void display(struct data *curr){
+	if(curr != 0){
+		display(curr->left);
+		printf("%d ", curr->val);
+		display(curr->right);
+	}
 }
 
 void find(struct data *curr, int val){
-	if(curr==NULL){
-		printf("%d is not found\n",val);
+	if(curr == 0){
+		printf("%d is NOT found in BST\n", val);
 		return;
 	}
-	else if(curr->val == val){
-		printf("%d is found\n",val);
+	
+	if(curr->val == val){
+		printf("%d is found in BST\n", val);
 		return;
 	}
-	//jika val lebih kecil dari curr node , rekursif ke kiri
-	else if(val < curr->val){
-		find(curr->left , val);
+	
+	if(val < curr->val){
+		find(curr->left, val);
 	}
-	//jika val lebih besar dari curr node , rekursif ke kanan
-	else if(val > curr->val){
+	
+	if(val > curr->val){
 		find(curr->right, val);
 	}
 }
 
-struct data *delRecursive(struct data *parent, struct data *curr){
-	// jika node leaf
-	if(curr->left == NULL && curr->right == NULL){
-		free(curr);
-		return 0 ; 
-	}
-	
-	// jika node hanya memiliki left child
-	else if(curr->left != NULL && curr->right == NULL){
-		return curr->left;
-		
-	}
-	
-	//jika node hanya memiliki right child
-	
-	else if(curr->left == NULL && curr->right!= NULL){
-		return  curr->right;
-	}
-	
-	//jika node memiliki 2 anak
+void peek(){
+	printf("%d\n", root->val);
 }
 
-struct data *parent = NULL;
+struct data *delRecursive(struct data *parentNodeToDel, struct data *nodeToDel){
+	//if nodeToDel is LEAF
+	if(nodeToDel->left == 0 && nodeToDel->right == 0){
+		//if there is only 1 node in BST
+		if(root == nodeToDel)
+			root = 0;
+		free(nodeToDel);
+		return 0;
+	}
+	
+	//if nodeToDel has ONLY LEFT child
+	else if(nodeToDel->left != 0 && nodeToDel->right == 0){
+		struct data *temp = nodeToDel->left;
+		if(nodeToDel->val < parentNodeToDel->val){
+			parentNodeToDel->left = temp;
+		}
+		else{
+			parentNodeToDel->right = temp;
+		}
+		return temp;
+	}
+	//if nodeToDel has ONLY RIGHT child
+	else if(nodeToDel->left == 0 && nodeToDel->right != 0){
+		struct data *temp = nodeToDel->right;
+		if(nodeToDel->val < parentNodeToDel->val){
+			parentNodeToDel->left = temp;
+		}
+		else{
+			parentNodeToDel->right = temp;
+		}
+		return temp;
+	}
+	//if nodeToDel has TWO children
+	else{
+		//find successor (rightmost child of left subtree)
+		struct data *t = nodeToDel->left;
+		struct data *tp = nodeToDel;
+		while(t->right){
+			tp = t;
+			t = t->right;
+		}
+		//copy value from t (successor) to node to be deleted
+		nodeToDel->val = t->val;
+		//delete successor node
+//		t = delRecursive(tp, t);
+		
+		if(tp->left == t){
+			tp->left = t->left;
+		}
+		else{
+			tp->right = tp->left;
+		}
+		
+		return nodeToDel;
+	}
+}
+
+struct data *parent = 0;
 void del(struct data *curr, int delVal){
-	if (curr == NULL){
-		printf("%d is not available to delete\n",delVal);
+	//if we found NULL spot / delVal is not in BST
+	if(curr == 0){
+		printf("%d is not available in BST\n", delVal);
 		return;
 	}
+	
+	//if we found delVal in BST
 	if(curr->val == delVal){
-		//no delete
-		curr = delRecursive(parent,curr);
-		if(delVal > parent->val) parent->right = curr;
-		else parent->left = curr;
+		//do deletion
+		curr = delRecursive(parent, curr);
+//		printf("%d\n", curr->val);
 		return;
 	}
+	
+	//if delVal is smaller than curr->val, recursive to left
 	if(delVal < curr->val){
-		parent =  curr;
-		del(curr->left , delVal);
+		parent = curr;
+		del(curr->left, delVal);
 	}
+	//if delVal is bigger than curr->val, recursive to right
 	else if(delVal > curr->val){
 		parent = curr;
-		del(curr->right , delVal);
+		del(curr->right, delVal);
 	}
 }
-
-
 
 int main(){
 	
-	insert(root, 20);
 	insert(root, 10);
-	insert(root, 50);
-	insert(root, 2);
-	insert(root, 100);
-	insert(root, 90);
-	insert(root, 75);
+	insert(root, 20);
+	insert(root, 9);
+	insert(root, 5);
+	insert(root, 11);
+	insert(root, 1);
+	insert(root, 12);
+	insert(root, 25);
+	insert(root, 30);
+	insert(root, 35);
+	insert(root, 40);
 	
-	displayInfix(root);
-	printf("\n");
-
-//	find(root, 5);
-//	find(root, 20);
-//	find(root, 101);
-
-	del(root,10);
-	del(root,11);
-	displayInfix(root);
+	display(root); printf("\n");
 	
+	del(root, 20);
+	display(root); printf("\n");
+	del(root, 10);
+	display(root); printf("\n");
+	del(root, 0);
+	display(root); printf("\n");
+	del(root, 5);
+	display(root); printf("\n");
+	del(root, 30);
+	display(root); printf("\n");
 	
-	return 0;
 }
